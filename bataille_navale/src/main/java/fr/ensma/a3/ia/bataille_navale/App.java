9461f68b701a947274a991f27d9dedb669a15208
Clement Geraud <sandust71@hotmail.fr>
2019-01-06 17:42:20 +0100

    Initial code commit

---
+++ bataille_navale/src/main/java/fr/ensma/a3/ia/bataille_navale/App.java
@@ -0,0 +1,13 @@
+package fr.ensma.a3.ia.bataille_navale;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Plouf !!" );
+    }
+}
---
+++ bataille_navale/src/main/java/fr/ensma/a3/ia/bataille_navale/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Base package for the game
+ */
+/**
+ * @author AxAlpha
+ *
+ */
+package fr.ensma.a3.ia.bataille_navale;
