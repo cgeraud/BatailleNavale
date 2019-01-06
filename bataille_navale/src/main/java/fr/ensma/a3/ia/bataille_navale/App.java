ddd28456f2431434673b40e524e3612a306de508
Simon <simon@Simon.com>
2019-01-06 23:38:50 +0100

    implemented little main testing

--- bataille_navale/src/main/java/fr/ensma/a3/ia/bataille_navale/App.java
+++ bataille_navale/src/main/java/fr/ensma/a3/ia/bataille_navale/App.java
@@ -1,5 +1,10 @@
 package fr.ensma.a3.ia.bataille_navale;
 
+import fr.ensma.a3.ia.bataille_navale.game_elements.Cruiser;
+import fr.ensma.a3.ia.bataille_navale.map.Map;
+import fr.ensma.a3.ia.bataille_navale.utils.Coordinates;
+import fr.ensma.a3.ia.bataille_navale.utils.Direction;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Plouf !!" );
+        Map mapPlayer1 = new Map();
+        Map mapPlayer2 = new Map();
+        Cruiser nimitz = new Cruiser(Direction.Horizontal, new Coordinates(0,0));
+        
     }
 }
