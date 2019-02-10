e15f232d61480e26448c0fb2481e446964972bf6
Clement Geraud <sandust71@hotmail.fr>
2019-02-10 11:31:57 +0100

    Initial commit

---
+++ bataille_navale/src/main/java/fr/ensma/a3/ia/bataille_navale/GameMaster/IPlayer.java
@@ -0,0 +1,31 @@
+package fr.ensma.a3.ia.bataille_navale.GameMaster;
+
+import fr.ensma.a3.ia.bataille_navale.GameMaster.Attacks.AttackOutOfMapException;
+import fr.ensma.a3.ia.bataille_navale.GameMaster.Attacks.IAttack;
+import fr.ensma.a3.ia.bataille_navale.game_elements.ShipIsDisabledException;
+import fr.ensma.a3.ia.bataille_navale.game_elements.Ships.ShipAlreadyExistsException;
+import fr.ensma.a3.ia.bataille_navale.game_elements.Ships.ShipCannotAttackException;
+import fr.ensma.a3.ia.bataille_navale.game_elements.Ships.ShipCannotFlareException;
+import fr.ensma.a3.ia.bataille_navale.game_elements.Ships.ShipOutOfMapException;
+import fr.ensma.a3.ia.bataille_navale.game_elements.Ships.ShipType;
+import fr.ensma.a3.ia.bataille_navale.game_elements.Ships.ShipsOverlappingException;
+import fr.ensma.a3.ia.bataille_navale.map.IMapOpponent;
+import fr.ensma.a3.ia.bataille_navale.map.ShipDoesNotExistException;
+import fr.ensma.a3.ia.bataille_navale.utils.Coordinates;
+import fr.ensma.a3.ia.bataille_navale.utils.Direction;
+
+public interface IPlayer {
+	// Player state methods
+	boolean playerIsalive();
+	int getTurnCoolDown();
+	void setTurnCoolDown(int val);
+	boolean isReady();
+	boolean isDisabled();
+	
+	// Player interaction methods
+	void setModeAttaque(IAttack mode);
+	IMapOpponent getMap();
+	void attack(IPlayer target, Coordinates coos, String idbateau) throws ShipIsDisabledException, ShipDoesNotExistException, AttackOutOfMapException, ShipCannotAttackException, ShipCannotFlareException;
+	void addNewShip(String id, ShipType type, Direction dir, Coordinates ref) throws ShipAlreadyExistsException, ShipOutOfMapException, ShipsOverlappingException, ShipDoesNotExistException;
+	void upgradeShipsResistance(String id, float dmgreduction) throws ShipDoesNotExistException;
+}
