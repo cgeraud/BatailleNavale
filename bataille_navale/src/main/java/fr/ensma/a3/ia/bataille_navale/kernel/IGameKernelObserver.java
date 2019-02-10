460d6c1722905a1ca3e68c43927bc7cc45c6f7e7
Clement Geraud <sandust71@hotmail.fr>
2019-02-10 15:11:32 +0100

    Created kernel observer interface

--- bataille_navale/src/main/java/fr/ensma/a3/ia/bataille_navale/kernel/IGameKernelObserver.java
+++ bataille_navale/src/main/java/fr/ensma/a3/ia/bataille_navale/kernel/IGameKernelObserver.java
@@ -1,5 +1,9 @@
 package fr.ensma.a3.ia.bataille_navale.kernel;
 
 public interface IGameKernelObserver {
-
+	void notifyPreGame();
+	void notifyPlayer1Turn();
+	void notifyPlayer2Turn();
+	void notifyPlayer1Won();
+	void notifyPlayer2Won();
 }
